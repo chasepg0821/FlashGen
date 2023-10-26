6dd38ccfbb2d4b5a1fe8e0a79dff26c5b0e07829
Chase Guynup <chasepg0821@gmail.com>
2023-10-26 10:20:43 -0400

    init

---
+++ src/components/FlashCard/FlashCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface FCProps {
+    prompt: string;
+    answer: string;
+    flipOnClick?: boolean;
+}
+
+const flipCard = () => {};
+
+const FlashCard = ({ prompt, answer, flipOnClick }: FCProps) => {
+    return <div>FlashCard</div>;
+};
+
+export default FlashCard;
