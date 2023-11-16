a026ca7ecd312cec8daf3cd06fa2720407b5f376
chasepg0821 <chasepg0821@gmail.com>
2023-11-16 08:27:30 -0500

    nav basis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,48 @@
-import { Steps } from "antd";
+import { FloatButton, Steps } from "antd";
 import React, { useState } from "react";
 import Welcome from "./sub-pages/Welcome";
+import { StepBackwardFilled, StepForwardFilled } from "@ant-design/icons";
+
+import style from "./home.module.css";
 
 const steps = [
     {
         title: "Welcome",
-        content: <Welcome />
+        content: <Welcome />,
+        description: "Brief intro and confirmation of eligibility."
     },
     {
         title: "Build Your Deck",
-        content: "Second-content"
+        content: "Second-content",
+        description: "Build a set of 10 cards."
     },
     {
         title: "Correctly Paraphrase",
-        content: "Last-content"
+        content: "Last-content",
+        description: "Give correct paraphrases."
     },
     {
         title: "Incorrectly Paraphrase",
-        content: "Last-content"
+        content: "Last-content",
+        description: "Give incorrect paraphrases."
     },
     {
         title: "Thank You!",
-        content: "Last-content"
+        content: "Last-content",
+        description: "The end of the study!"
     }
 ];
 
 const items = steps.map((item) => ({
     key: item.title,
-    title: item.title
+    title: item.title,
+    description: item.description
 }));
 
-const Home = ({ children }) => {
-    const [stepNo, setStepNo] = useState(0);
-    const [canAdvance, setCanAdvance] = useState(false);
+const Home = () => {
+    const [stepNo, setStepNo] = useState(1);
+    const [stepStatus, setStepStatus] = useState("process");
+    const [canAdvance, setCanAdvance] = useState(true);
 
     const handleChangeAdvancable = () => {
         setCanAdvance(!canAdvance);
@@ -45,20 +55,54 @@ const Home = ({ children }) => {
 
     const prev = () => {
         setStepNo(stepNo - 1);
+        setCanAdvance(false);
+    };
+
+    const handleStepStatus = (status) => {
+        setStepStatus(status);
     };
 
     const renderNavButtons = () => {
         return (
-            <div className="nav-button-container">
-                {canAdvance && <button onClick={next}>Next</button>}
-                {stepNo > 0 && <button onClick={prev}>Previous</button>}
-            </div>
+            <>
+                {canAdvance && (
+                    <FloatButton
+                        icon={<StepForwardFilled />}
+                        shape="square"
+                        onClick={next}
+                        type="primary"
+                        description="Next"
+                        style={{
+                            width: 128
+                        }}
+                    />
+                )}
+                {stepNo > 0 && (
+                    <FloatButton
+                        icon={<StepBackwardFilled />}
+                        shape="square"
+                        onClick={prev}
+                        style={{
+                            right: canAdvance ? 24 + 138 : 24,
+                            width: 128
+                        }}
+                        description="Back"
+                    />
+                )}
+            </>
         );
     };
     return (
-        <div>
-            <Steps current={stepNo} items={items} />
-            {children}
+        <div className={style.page_window}>
+            <Steps
+                status={stepStatus}
+                current={stepNo}
+                items={items}
+                className={style.custom_steps}
+            />
+            <div className={style.content_window}>{steps[stepNo].content}</div>
+            {renderNavButtons()}
+            <FloatButton.BackTop shape="square" style={{ left: 24 }} />
         </div>
     );
 };
--- src/pages/sub-pages/Welcome.jsx
+++ src/pages/sub-pages/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Welcome = () => {
-    return <div>Welcome</div>;
+    return <div style={{ height: 5400 }}>Welcome</div>;
 };
 
 export default Welcome;
