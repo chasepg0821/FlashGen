186270e544cac7979817ea4eb72bc95a9bdd47db
chasepg0821 <chasepg0821@gmail.com>
2023-11-23 13:34:57 -0500

    hf cnx

--- api.py
+++ api.py
@@ -7,23 +7,20 @@ api = Blueprint('api', __name__)
 
 cnx = mysql.connector.connect(user=os.environ["AZURE_MYSQL_USER"], password=os.environ["AZURE_MYSQL_PASSWORD"], host=os.environ["AZURE_MYSQL_HOST"], port=3306, database=os.environ["AZURE_MYSQL_NAME"], ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
 
+token = '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'
+
 @api.route('/make-comparison', methods=['POST'])
 def compare ():
-    body = {
-        "inputs": [
-            {
-                "text": "hi",
-                "text_pair": "hello"
-             }
-        ]
-    }
+    body = request.json
+
+    print(body)
 
     API_URL = "https://ts6ke7jk9qtdxz1g.us-east-1.aws.endpoints.huggingface.cloud"
     headers = {
-        "Authorization": f"Bearer {os.environ['HF_TOKEN']}",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
 
     response = requests.post(API_URL, headers=headers, json=body)
 
-    return response
+    return response.json()
