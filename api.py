4fe30ba836c7877f8a65cc57d29b81edbb2cb5d2
chasepg0821 <chasepg0821@gmail.com>
2023-11-23 12:20:45 -0500

    test cnx

--- api.py
+++ api.py
@@ -1,10 +1,26 @@
+import os
 from flask import Blueprint, request, jsonify
+import mysql.connector
 
 api = Blueprint('api', __name__)
 
+cnx = mysql.connector.connect(user=os.environ["AZURE_MYSQL_USER"], password=os.environ["AZURE_MYSQL_PASSWORD"], host=os.environ["AZURE_MYSQL_HOST"], port=3306, database=os.environ["AZURE_MYSQL_NAME"], ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
+
 @api.route('/make-comparison', methods=['POST', 'GET'])
 def compare ():
+    cursor = cnx.cursor()
+
+    query = ("SELECT * FROM pairs")
+
+    cursor.execute(query)
+
+    rows = []
+
+    for row in cursor:
+        rows.append(row)
+
+    cursor.close()
 
     return {
-        "response" : "hi"
+        "response" : rows
     }
