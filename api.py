c6f4eb5b96ed19da241ce26b552f09410a764314
chasepg0821 <chasepg0821@gmail.com>
2024-02-06 16:40:50 -0500

    modification for no env

--- api.py
+++ api.py
@@ -6,110 +6,42 @@ import uuid
 
 api = Blueprint('api', __name__)
 
-def connect():
-    return mysql.connector.connect(user=os.environ["AZURE_MYSQL_USER"], password=os.environ["AZURE_MYSQL_PASSWORD"], host=os.environ["AZURE_MYSQL_HOST"], port=3306, database=os.environ["AZURE_MYSQL_NAME"], ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
+# def connect():
+#     return mysql.connector.connect(user=os.environ[{username}], password=os.environ[{password}], host=os.environ[{host}], port=3306, database=os.environ[{db}], ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
 
-"""
-Request:
-{
-    "cards" : [
-        ...cards
-    ]
-}
-Response:
-{
-    "pairs" : [
-        ...inputPairs
-    ],
-    "evaluations" : [
-        ...evaluationsOfPairs
-    ]
-}
-Params:
-    key - ("correctParaphrase", "incorrectParaphrase")
-"""
-@api.route('/make-comparison', methods=['POST'])
-def compare ():
-    cards = request.json['cards']
-    key = request.args.get('key')
+## EXAMPLE PYTHON CODE TO MAKE QUERIES ##
 
-    inputs = []
-    for card in cards:
-        inputs.append({
-            "text" : card['answer'],
-            "text_pair" : card[key]
-        })
+# cnx = connect()
+# cursor = cnx.cursor()
 
-    API_URL = "https://ts6ke7jk9qtdxz1g.us-east-1.aws.endpoints.huggingface.cloud"
-    headers = {
-        "Authorization": f'Bearer {os.environ["HF_TOKEN"]}',
-        "Content-Type": "application/json"
-    }
+# query = "INSERT INTO pairs (session_id, text, text_pair, label, model_result) VALUES (%s, %s, %s, %s, %s)"
 
-    response = requests.post(API_URL, headers=headers, json={
-        "inputs" : inputs
-    })
+# cursor.executemany(query, data)
+# rows = cursor.rowcount
 
-    return response.json(), response.status_code
+# cnx.commit()
+# cursor.close()
+# cnx.close()
 
-"""
-Request: GET
-"""
-@api.route('/ping-model', methods=['GET'])
-def ping ():
-    API_URL = "https://ts6ke7jk9qtdxz1g.us-east-1.aws.endpoints.huggingface.cloud"
-    headers = {
-        "Authorization": f'Bearer {os.environ["HF_TOKEN"]}',
-    }
 
-    requests.get(API_URL, headers=headers)
+return {
+    "message" : f"Rows edited: {rows}"
+}, 200
+@api.route('/example', methods=['POST', 'GET'])
+def example ():
+    if request.method == 'GET':
+        reqArg = request.args.get("key")
 
-    return {
-        "message" : "ping"
-    }, 200
+        return {
+            "resBody": reqArg
+        }, 200
+    else:
+        reqBody = request.json
 
-"""
-Request:
-{
-    "cards" : [
-        ...cards
-    ],
-    "CPEvals" : [
-        ...correctParaphraseEvals
-    ],
-    "IPEvals" : [
-        ...incorrectParaphraseEvals
-    ]
-}
-"""
-@api.route('/send-pairs', methods=['POST'])
-def sendPairs ():
-    cards = request.json["cards"]
-    cpEvals = request.json["CPEvals"]
-    ipEvals = request.json["IPEvals"]
+        return {
+            "resBody": reqBody
+        }, 200
 
-    sID = str(uuid.uuid4())
 
-    data = []
-    for i, card in enumerate(cards):
-        data.append([sID, card['answer'], card['correctParaphrase'], 1, cpEvals[i]])
-        data.append([sID, card['answer'], card['incorrectParaphrase'], 0, ipEvals[i]])
-
-    cnx = connect()
-    cursor = cnx.cursor()
-
-    query = "INSERT INTO pairs (session_id, text, text_pair, label, model_result) VALUES (%s, %s, %s, %s, %s)"
-
-    cursor.executemany(query, data)
-    rows = cursor.rowcount
-
-    cnx.commit()
-    cursor.close()
-    cnx.close()
-    
-
-    return {
-        "message" : f"Rows edited: {rows}"
-    }, 200
 
 
